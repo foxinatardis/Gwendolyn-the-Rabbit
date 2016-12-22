7174198b3f9eebdfd388e0bcd11dc8188978f001
foxinatardis <foxinatardis@gmail.com>
2016-12-21 17:54:03 -0700

    still debugging heroku

--- index.js
+++ index.js
@@ -2,13 +2,13 @@
 
 var express = require('express');
 var app = express();
-var PORT = process.env.PORT;
+var PORT = process.env.PORT || 5000;
+
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html');
 });
-
-app.use(express.static(__dirname + '/public'));
 
 app.listen(PORT, () => {
 	console.log("Server started on port " + PORT);
